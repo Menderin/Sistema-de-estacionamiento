9446ec4cbe75c848bfc09e40684253ff861f9755
Liespren <kevin.silva@alumnos.ucn>
2026-07-05 22:05:28 -0400

    Limpiar rastreo de dist

--- dist/scripts/config.js
+++
@@ -1,9 +0,0 @@
-// scripts/config.js
-// Configuración para Producción (Railway)
-export const API_BASE = "https://sistema-de-estacionamiento-production-7de7.up.railway.app/api";
-
-/*
-// Si quieres usarlo en local, comenta la línea de arriba y usa esta:
-const hostname = window.location.hostname;
-export const API_BASE = `http://${hostname}:8000/api`;
-*/
